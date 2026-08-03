c7a7c4af69d302aa5b199b523a6f6412f2228fbc
h-morimoto-gs <h.morimoto@genbasupport.com>
2026-08-03 11:23:58 +0900

    レビュー修正（DateをRangeに変更）

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -12,18 +12,15 @@ opt.on("-y YEAR", Integer) {|v| year = v}
 opt.on("-m MONTH", Integer) {|v| month = v}
 opt.parse!(ARGV)
 
-last_date = Date.new(year, month, -1)
-days_in_month = last_date.day
-first_wday = Date.new(year, month, 1).wday
+first_date = Date.new(year, month, 1)
+last_date  = Date.new(year, month, -1)
 
 puts "#{month}月 #{year}".center(20)
 puts "日 月 火 水 木 金 土"
-print "   " * first_wday
+print "   " * first_date.wday
 
-(1..days_in_month).each do |day|
-  date = Date.new(year, month, day)
-
-  print day.to_s.rjust(2), " "
+(first_date..last_date).each do |date|
+  print date.day.to_s.rjust(2), " "
   puts "" if date.saturday?
 end
 puts "" unless last_date.saturday?
