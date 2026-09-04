c36c65a97e1b019a3652ec7ebfd4129a4da5e11b
h-morimoto-gs <h.morimoto@genbasupport.com>
2026-09-04 11:41:13 +0900

    nil判定を三項演算子に変更

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -7,8 +7,8 @@ class Frame
 
   def initialize(first_mark, second_mark = nil, third_mark = nil)
     @first_shot = Shot.new(first_mark)
-    @second_shot = second_mark && Shot.new(second_mark)
-    @third_shot = third_mark && Shot.new(third_mark)
+    @second_shot = second_mark.nil? ? nil : Shot.new(second_mark)
+    @third_shot = third_mark.nil? ? nil : Shot.new(third_mark)
   end
 
   def score
